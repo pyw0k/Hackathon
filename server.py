7dfc5510373e465e0533d571e99ae96c03ed6f1f
pyw0k <adrianpe92@gmail.com>
2017-10-14 15:49:41 -0700

    Added search,login,remember me

--- server.py
+++ server.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 mysql = MySQLConnector(app,'reddit')
 @app.route('/')
 def index():
-    query = "SELECT * FROM reddit"
+    query = "SELECT * FROM users"
     users = mysql.query_db(query)
     return render_template('index.html', all_users=users)
 @app.route('/usersreg', methods=['POST'])
 def create():
 	query = "INSERT INTO users(username, password, created_at,updated_at) VALUES(:users, :password, NOW(),NOW())"
-	data= {
-		'username': request.form['username']
-		'password': request.form['password']
-		  }
+  # data=  {
+    #         'username': request.form['username'],
+             #'password':  request.form['password']
+       #    }
 	mysql.query_db(query, data)
-    return redirect('/')
+	return redirect('/')
 app.run(debug=True)
