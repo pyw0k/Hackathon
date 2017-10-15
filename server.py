8fcdb01249a407c4ae18fecadd5606f1a6567cf1
pyw0k <adrianpe92@gmail.com>
2017-10-14 17:50:47 -0700

    Added /create route in server.py, skeletons of html pages

--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
+
 app = Flask(__name__)
 mysql = MySQLConnector(app,'reddit')
+
 @app.route('/')
 def index():
     query = "SELECT * FROM users"
     users = mysql.query_db(query)
     return render_template('index.html', all_users=users)
-@app.route('/usersreg', methods=['POST'])
-def create():
-	query = "INSERT INTO users(username, password, created_at,updated_at) VALUES(:users, :password, NOW(),NOW())"
-  # data=  {
-    #         'username': request.form['username'],
-             #'password':  request.form['password']
-       #    }
-	mysql.query_db(query, data)
+
+@app.route('/create', methods=['POST'])
+def create_user():
+     username = request.form['username']
+     password = md5.new(request.form['password']).hexdigest()
+     #passwordconfirm = md5.new(request.form ['password'].hexdigest())
+     insert_query = "INSERT INTO users (username, password, created_at, updated_at) VALUES (:username, :password, NOW(), NOW())"
+     query_data = { 'username': username, 'password': password }
+     mysql.query_db(insert_query, query_data)
 	return redirect('/')
+
+
+#@app.route('/reset', methods = ['POST'])
+#def reset():
+
+
+#@app.route('')
 app.run(debug=True)
