f17cf3fd0a436bd8449c5d4a8cf14b9d5f989d78
pyw0k <adrianpe92@gmail.com>
2017-10-15 15:04:58 -0700

    Included Maddi's segments to python and html

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
+import md5
 
 app = Flask(__name__)
 mysql = MySQLConnector(app,'reddit')
@@ -21,6 +22,38 @@ def create_user():
     return redirect('/create')
 #@app.route('/reset', methods = ['POST'])
 #def reset():
+
+@app.route('/login', methods=['post'])
+def longinprocess():
+	username = request.form['username']
+	password = md5.new(request.form['password']).hexdigest()
+	user_query = "SELECT * FROM users where users.username = :username"
+	query_data = { 'username': username}
+	user = mysql.query_db(user_query, query_data)
+
+	print user #changed from users to user/ Line 32
+	EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+	# invalid username
+	if len(request.form['username'])<1:
+		flash("Valid Email format!")
+		return redirect('/login')
+	# user doesn't exists
+	elif len(request.form['password']) <1:
+		flash("no password")
+		return redirect('/login')
+	# invalid user
+	elif  not user: #this was user to begin with????
+		flash("Wrong email")
+		return redirect('/login')
+	# invalid password
+	elif user[0]['password'] != password:
+		flash("Wrong password")
+		return redirect('/login') #redirect('/login_page')
+	# correct info
+	else:
+		session['id'] = user[0]['id']
+		return redirect ('/welcome')
+
 
 
 #@app.route('')
