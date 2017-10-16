e72d2ff420a543077afd05125c2ecc0798ba3ae4
pyw0k <adrianpe92@gmail.com>
2017-10-16 08:48:03 -0700

    Bleep

--- server.py
+++ server.py
@@ -6,85 +6,68 @@ import re
 app = Flask(__name__)
 mysql = MySQLConnector(app,'reddit')
 app.secret_key = "ThisIsSecret!"
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+USERNAME_REGEX= re.compile(r'[a-zA-Z0-9.+_-]')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    query = "SELECT * FROM users"
-    users = mysql.query_db(query)
-    return render_template('index.html', all_users=users)
+    if 'user_id' in session and 'username' in session:
+        return redirect('/user')
+# -- subreddit posts --
+    subpost_query="SELECT posts.text, posts.created_at, users.username, subreddits.url FROM posts JOIN users ON posts.user_id=users.id JOIN subreddits ON posts.subreddit_id=subreddit_id LIMIT 0,50"
+    subposts=mysql.query_db(subpost_query)
+
+    return render_template('index.html', all_subposts=subposts)
+
 
 @app.route('/create', methods=['POST'])
 def create_user():
-    username = request.form['username'] #username= login username 
-    password = request.form['password']
-#if username ---   
-    if len(request.form['username']) < 2:  
-        flash('Username name cannot be blank!')
-        return redirect('/')
-    elif not NAME_REGEX.match(username): # if invalid format
-        flash('Invalid format')
-        return redirect('/')
-#password---
-    if len(request.form['password']) < 8: #less than 2 letters 
-        flash('password needs to be more than 8!')
-        return redirect('/')
-    if (request.form['password']) != request.form['confirmpassword']:
-        flash('password is not matched!')
-    else: 
-        password = md5.new(request.form['password']).hexdigest()
-        insert_query = "INSERT INTO users (username, password) VALUES (:username, :password )"
-        query_data = { 'username': username, 'password': password }
-        mysql.query_db(insert_query, query_data)
-        flash ('Thanks!')
-    return redirect('/')
-    
-
-
-
-
-@app.route('/login', methods=['post'])
-def longinprocess():
-	# username = request.form['username']
-
-    hash_pass = md5.new(request.form['password']).hexdigest()
-    print "password: ", hash_pass
-    # query1 = "SELECT username, password FROM users WHERE username = :username AND password = :password"
-    # data1 = { "username": request.form['username'], "password": hash_pass}
-    query2 = "SELECT * FROM users where users.username = :username"
-    data2 = { 'users.username': username }  
-    #user = mysql.query_db(user_query, query_data)
-    user = mysql.query_db(query2, data2)
-
-    # show_query = mysql.query_db(query1, data1)
-    # users = mysql.query_db(query2, data2)
-
-    # user_query = "SELECT * FROM users where users.username = :username"
-	# query_data = { 'username': username}
-    # user = mysql.query_db(user_query, query_data)
-    # print "show_query: ", show_query ------how to print out 
-   
-    if show_query ==[]:
-        flash('Wrong login information')
-        return redirect('/')      
+    if len(request.form['username'])<1:
+        flash(u"Username cannot be blank!","registration")
+    if len(request.form['password'])<8:
+        flash(u"Password must be at least 8 characters long","registration")
+    if request.form['confirm_password']==request.form['password']:
+        flash(u"Password does not match","registration")
+    if not USERNAME_REGEX.match(request.form['username']):
+        flash(u"Username can only contain alphanumerical and/or.+-_ characters","registration")
     else:
-        session['hold']= users.id[0]['id'] #holdsession
-        return redirect('/welcome')
- 
+        username= request.form['username']
+        password= md5.new(request.form['password']).hexdigest()
+        insert_query="INSERT INTO users (username,password) VALUES (:username, :password)"
+        query_data={'username':username, 'password':password}
+        mysql.query_db(insert_query,query_data)
+        return redirect('/user')
+    return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username=request.form['username']
+    password = md5.new(request.form['password']).hexdigest()
+    user_query= "SELECT * FROM users WHERE users.username= :username AND users.password=:password"
+    query_data={'username':username, 'password':password}
+    user= mysql.query_db(user_query,query_data)
+    if user:
+        session['user_id']=int(user[0]['id'])
+        session['username']=int(user[0]['id'])
+        return redirect('/user')
+    else:
+        flash(u"User email or password invalid","login")
+    return redirect('/')
+
+
 @app.route('/post', methods =['POST'])
 def post_message():
     if 'hold' in session:
         name_query = 'SELECT * FROM users WHERE id = :id'
         id_data = { 'id': session['hold'] }
         id_user = mysql.query_db(name_query,id_data)
-        username = id_user[0]['username']
+        username = id_user[0]['firstname']
 
         query_post = 'SELECT posts.id, text, posts.created_at, posts.updated_at, users.id as author_id, users.username FROM posts JOIN users ON posts.user_id = users.id ORDER BY created_at DESC'
         query_comment = 'SELECT comments.id, posts_id, comment,comments.created_at,users.id as author_id,users.username FROM comments JOIN users ON comments.user_id = users.id'
         posts = mysql.query_db(query_post)
         comments = mysql.query_db(query_comment)
-        return render_template('post.html', username = username, posts = posts, comments = comments)
+        return render_template('post.html', id = users.id, posts = posts, comments = comments)
     else:
         return redirect('/post')
 @app.route('/posts_post', methods =['POST'])
@@ -112,8 +95,17 @@ def post_comment():
 
     return redirect('/post')
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id',None)
+    session.pop('username', None)
+    return redirect('/')
 
-
+@app.route('/user')
+def userdash():
+    subpost_query="SELECT posts.text, posts.created_at, users.username, subreddits.url FROM posts JOIN users ON posts.user_id=users.id JOIN subreddits ON posts.subreddit_id=subreddit_id LIMIT 0,50"
+    subposts=mysql.query_db(subpost_query)
+    return render_template('user.html', all_subposts=subposts)
 # @app.route('/add_subred', methods=['post'])
 # def add_subred():
 #         subreddit = request.form['subreddit']
